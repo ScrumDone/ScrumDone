b54e7244d70471321a712b79925a90ad53bd3929
Aleksander Radecki <alerksanderradecki@gmail.com>
2026-04-13 14:40:19 +0200

    added more developing

--- client/src/pages/Homepage.tsx
+++ client/src/pages/Homepage.tsx
@@ -15,9 +15,30 @@ const Homepage: React.FC = () => {
                     <h1 className="font-segoe-ui text-black text-[1.5rem] leading-8 font-normal tracking-[0em] antialiased mb-6">Strona Główna</h1>
 
                     <div className="grid grid-cols-3 gap-6">
-                        <div className="bg-white rounded-xl border border-gray-200 p-4">...</div>
-                        <div className="bg-white rounded-xl border border-gray-200 p-4">...</div>
-                        <div className="bg-white rounded-xl border border-gray-200 p-4">...</div>
+                        <div className="bg-white rounded-xl border border-gray-200 p-6">
+                            <h1 className="font-segoe-ui  text-slate-800 text-[1.125rem] leading-7 font-normal tracking-[0em] antialiased mb-6">
+                                Dzisiaj
+                            </h1>
+                            <div className="border border-gray-200 rounded-lg p-4">
+                                <h1 className="font-segoe-ui  text-slate-800 text-[0.875rem] leading-7 font-normal tracking-[0em] antialiased mb-6">Quotes Generation Module</h1>
+                            </div>
+                        </div>
+                        <div className="bg-white rounded-xl border border-gray-200 p-6">
+                            <h1 className="font-segoe-ui text-slate-800 text-[1.125rem] leading-7 font-normal tracking-[0em] antialiased mb-6">
+                                Aktualności
+                            </h1>
+                            <div className="border border-gray-200 rounded-lg p-4">
+
+                            </div>
+                        </div>
+                        <div className="bg-white rounded-xl border border-gray-200 p-6">
+                            <h1 className="font-segoe-ui text-slate-800 text-[1.125rem] leading-7 font-normal tracking-[0em] antialiased mb-6">
+                                Projekty
+                            </h1>
+                            <div className="border border-gray-200 rounded-lg p-4">
+
+                            </div>
+                        </div>
                     </div>
                 </div>
             </main>
