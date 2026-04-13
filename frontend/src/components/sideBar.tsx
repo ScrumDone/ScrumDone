c435d56d53949631fadde10adbeeee2fc583aa56
Aleksander Radecki <alerksanderradecki@gmail.com>
2026-04-13 10:44:44 +0200

    first tab

--- frontend/src/components/sideBar.tsx
+++ frontend/src/components/sideBar.tsx
@@ -4,8 +4,9 @@ import React from 'react'
 const SideBar: React.FC = () => {
     return (
         <aside className="fixed inset-y-0 left-0 h-screen w-64 overflow-hidden border-r border-slate-200 bg-white">
-            <div className="h-full p-6">
-                <h1 className="font-segoe-ui text-scrumdone-blue-main text-[16px] leading-6 font-normal tracking-[0em] antialiased">ScrumDone</h1>
+            <div className="h-full p-6 flex gap-2">
+                <h1 className="font-segoe-ui text-scrumdone-blue-main text-[1rem] leading-6 font-normal tracking-[0em] antialiased">randlab</h1>
+                <h1 className="font-segoe-ui text-black text-[1rem] leading-6 font-normal tracking-[0em] antialiased">.pl</h1>
             </div>
         </aside>
     )
