c9d184cd184c22cad2481426991acba7bc1864f1
Aleksander Radecki <alerksanderradecki@gmail.com>
2026-04-13 09:57:18 +0200

    init app.tsx and homepage

---
+++ frontend/src/pages/Homepage.tsx
@@ -0,0 +1,13 @@
+
+import React from 'react'
+
+const Homepage: React.FC = () => {
+    return (
+        <div>
+            <h1>Welcome to ScrumDone!</h1>
+            <p>Your ultimate tool for managing Scrum projects efficiently.</p>
+        </div>
+    )
+}
+
+export default Homepage 
