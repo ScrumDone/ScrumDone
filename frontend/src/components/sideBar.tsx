1ee7f5e17e1311b11f5d674a9a9c7a732ae21676
Aleksander Radecki <alerksanderradecki@gmail.com>
2026-04-13 10:41:47 +0200

    added font segoue-ui

--- frontend/src/components/sideBar.tsx
+++ frontend/src/components/sideBar.tsx
@@ -4,8 +4,8 @@ import React from 'react'
 const SideBar: React.FC = () => {
     return (
         <aside className="fixed inset-y-0 left-0 h-screen w-64 overflow-hidden border-r border-slate-200 bg-white">
-            <div className="h-full p-4">
-                Sidebar
+            <div className="h-full p-6">
+                <h1 className="font-segoe-ui text-scrumdone-blue-main text-[16px] leading-6 font-normal tracking-[0em] antialiased">ScrumDone</h1>
             </div>
         </aside>
     )
