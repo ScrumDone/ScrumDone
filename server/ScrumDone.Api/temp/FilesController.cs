24411063c0390a7d8d902c002bdfd736840ac1d9
Joneck <eryk.dziemidzionek@gmail.com>
2026-05-05 22:30:14 +0200

    Add Controllers

---
+++ server/ScrumDone.Api/temp/FilesController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ScrumDone.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FilesController : ControllerBase
+    {
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status501NotImplemented)]
+        public async Task<IActionResult> GetFiles([FromQuery] FileQueryDto query)
+        {
+            return StatusCode(StatusCodes.Status501NotImplemented);
+        }
+    }
+}
---
+++ server/ScrumDone.Api/temp/UsersController.cs
@@ -0,0 +1,17 @@
+// using Microsoft.AspNetCore.Http;
+// using Microsoft.AspNetCore.Mvc;
+
+// namespace ScrumDone.Api.Controllers
+// {
+//     [Route("api/[controller]")]
+//     [ApiController]
+//     public class UsersController : ControllerBase
+//     {
+//         [HttpGet]
+//         [ProducesResponseType(StatusCodes.Status501NotImplemented)]
+//         public async Task<IActionResult> GetFiles([FromQuery] UserQueryDto query)
+//         {
+//             return StatusCode(StatusCodes.Status501NotImplemented);
+//         }
+//     }
+// }
