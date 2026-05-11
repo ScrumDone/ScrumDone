d8dedec9d254787d32505fdc9e51f914d63d7f5e
Wojciech-Kalota <wojciech.kalota1@gmail.com>
2026-05-11 13:15:51 +0200

    Initial commit

--- server/ScrumDone.Api/Dto/UserDto.cs
+++
@@ -1,8 +0,0 @@
-public class UserDto
-{
-    public Guid Id { get; set; }
-
-    public string Name { get; set; } = "Marek";
-
-    public string ProfilePictureUrl { get; set; } = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQPvPTnLsfb2xLrQDC_Ug5Fnbrm690dzQsRYw&s";
-}
