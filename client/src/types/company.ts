2b5b14763d45e430a3eb48d7a98c8f0272347fac
Aleksander Radecki <alerksanderradecki@gmail.com>
2026-06-05 21:35:30 +0200

    added types

--- client/src/types/company.ts
+++ client/src/types/company.ts
@@ -6,7 +6,7 @@ export type CompanyListItem = {
   nip: string | null;
   createdAt: string;
   updatedAt: string;
-  mainContact: ContactPerson;
+  mainContact: ContactPerson | null;
   contactPeopleCount: number;
   projectsCount: number;
 };
@@ -18,4 +18,28 @@ export type CompanyUpdateDto = {
   krs: string | null;
   regon: string | null;
   address: string | null;
+};
+
+
+export type CompanyCreateDto = {
+  name: string;                     
+  nip: string | null;
+  krs: string | null;
+  regon: string | null;
+  address: string | null;
+};
+
+// odpowiedz z backendu do getCompanyDetails rozniu sie od CompanyListItem
+export type CompanyDetail = {
+  id: string;
+  name: string;
+  nip: string | null;
+  krs: string | null;
+  regon: string | null;
+  address: string | null;
+  createdAt: string;
+  updatedAt: string;
+  contactPeopleCount: number;
+  projectCount: number;
+  contacts: ContactPerson[];
 };
