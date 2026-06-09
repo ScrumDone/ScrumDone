b97546c4994d6d571fb3aee787aacd4969fcadde
Joneck <eryk.dziemidzionek@gmail.com>
2026-06-09 15:15:24 +0200

    Add validators for logs

---
+++ server/ScrumDone.Api/Validators/CompanyLogs/CompanyNoteCreateDtoValidator.cs
@@ -0,0 +1,24 @@
+﻿using FluentValidation;
+using ScrumDone.Api.DTOs.Companies;
+
+namespace ScrumDone.Api.Validators.Companies
+{
+    public sealed class CompanyLogCreateDtoValidator : AbstractValidator<CooperationLogCreateDto>
+    {
+        public CompanyLogCreateDtoValidator()
+        {
+            RuleFor(x => x.Title)
+                .NotEmpty()
+                .MaximumLength(50);
+
+            RuleFor(x => x.Description)
+                .MaximumLength(400);
+
+            RuleFor(x => x.OldValue)
+                .MaximumLength(100);
+
+            RuleFor(x => x.NewValue)
+                .MaximumLength(100);
+        }
+    }
+}
